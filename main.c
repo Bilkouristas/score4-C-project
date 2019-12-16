6fa1a2e48c880f237b6db37625a9a81492905c80
BIlkouristas <baskour28@gmail.com>
2019-12-16 03:37:00 +0200

    First updated main function

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*  test program  */
+int main(){
+
+	double x;
+	double y;
+
+
+	printf("Give me number 1: ");
+	scanf("%lf", &x);
+	printf("Enter number 2: ");
+	scanf("%lf", &y);
+
+	printf("The sum of them is: %f", x+y);
+
+
+
+
+
+
+
+
+	printf("\n");
+	return 0;
+
+
+}
